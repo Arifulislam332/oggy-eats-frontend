3f0573651aeb15179a39c6fe35fee85f91c57f3d
Arifulislam332 <ayatullaharif332@gmail.com>
2024-04-07 15:00:16 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+const HomePage = () => {
+  return <div>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut voluptatem consequatur laboriosam dolor voluptatum cum, aliquid sapiente ipsa at odit, illum non assumenda placeat vel officiis eum in, exercitationem nulla?
+  </div>;
+};
+
+export default HomePage;
